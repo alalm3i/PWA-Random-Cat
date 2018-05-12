48994b93b82ddb9038329438e0c022525da54225
Nabeel_taresh <Nabeel_taresh@bitbucket.org>
2018-05-12 16:50:35 +0300

    clean  mewo

--- js/app.js
+++ js/app.js
@@ -1,22 +1,15 @@
-const apiKey = "DTw3Z2l7DDzJjn7G";
-const _lat = "26.392666"; 
-const _long = "49.977714";
-const todayDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-const apiUrl = "http://api.islamhouse.com/v1/" + apiKey + "/services/praytime/get-times/" + todayDate + "/Makkah/" + _lat + "/" + _long + "/+3/json";
 
-// Get main sectin to push the result
-const main = document.querySelector("main");
 window.addEventListener('load', e =>{
-    updatePrayTimes();
+    updateRandomMeow();
 });
 
-async function updatePrayTimes() {
-    const res = await fetch(apiUrl);
-    const json = await res.json();
-
-    main.innerHTML = json.times.map(createTimeTable).join("\n");
-}
-
-function createTimeTable(times){
-    return times;
+async function updateRandomMeow() {
+    await fetch('https://aws.random.cat/meow')
+    .then(async function(response) {
+      return await response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson.file);
+      document.getElementById("meow").src=myJson.file;
+    });
 }
