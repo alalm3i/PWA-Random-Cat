aedfbeb1b4163cbcfd8fcf597c7488d4ea56f722
Nabeel_taresh <Nabeel_taresh@bitbucket.org>
2018-05-12 15:52:32 +0300

    change fetch method  mewo

--- js/app2.js
+++ js/app2.js
@@ -1,26 +1,17 @@
-const apiKey = "fc8ace5fdcde5f9f4f2ec023adba2077";
-const _lat = "26.392666"; 
-const _long = "49.977714";
-const todayDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-const apiUrl = "http://api.islamhouse.com/v1/" + apiKey + "/services/praytime/get-times/" + todayDate + "/Makkah/" + _lat + "/" + _long + "/+3/json";
 
-// Get main sectin to push the result
-const main = document.querySelector("main");
 window.addEventListener('load', e =>{
-    updatePrayTimes();
+    updateRandomMeow();
 });
 
-async function updatePrayTimes() {
-    //const res = await fetch("https://aws.random.cat/meow");
-    //const json = await res.json();
+async function updateRandomMeow() {
     await fetch('https://aws.random.cat/meow')
     .then(async function(response) {
       return await response.json();
     })
     .then(function(myJson) {
-      console.log(myJson);
+      console.log(myJson.file);
+     // document.getElementById("meow").src=myJson.file;
     });
-    //main.innerHTML = json.file.join("\n");
 }
 
 function createTimeTable(times){
