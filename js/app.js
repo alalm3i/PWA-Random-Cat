d6bbfd49d9739f482709514d354fe486e834a2a8
Nabeel_taresh <Nabeel_taresh@bitbucket.org>
2018-05-12 11:50:00 +0300

    First Commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const apiKey = "DTw3Z2l7DDzJjn7G";
+const _lat = "26.392666"; 
+const _long = "49.977714";
+const todayDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+const apiUrl = "http://api.islamhouse.com/v1/" + apiKey + "/services/praytime/get-times/" + todayDate + "/Makkah/" + _lat + "/" + _long + "/+3/json";
+
+// Get main sectin to push the result
+const main = document.querySelector("main");
+window.addEventListener('load', e =>{
+    updatePrayTimes();
+});
+
+async function updatePrayTimes() {
+    const res = await fetch(apiUrl,{
+        method: 'GET',
+        headers:{
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials':true,
+          'Access-Control-Allow-Methods':'POST, GET'
+        }
+    });
+    const json = await res.json();
+
+    main.innerHTML = json.times.map(createTimeTable).join("\n");
+}
+
+function createTimeTable(times){
+    return times;
+}
